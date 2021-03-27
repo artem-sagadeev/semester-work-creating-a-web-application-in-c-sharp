f2090f141a9c94a2636fe459063fce5d590b8419
Артём Сагадеев <sagadeev.artem2001@gmail.com>
2021-03-27 14:25:12 +0300

    add identity auth

---
+++ src/WebApp/Pages/Developers/Index.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages.Developers
+{
+    public class Index : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
