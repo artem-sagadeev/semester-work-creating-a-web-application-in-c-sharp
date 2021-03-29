09631f98f736c144ea68bc0b4890ac8074b427ba
Артём Сагадеев <sagadeev.artem2001@gmail.com>
2021-03-29 13:57:13 +0300

    add user details page

--- src/WebApp/Pages/Users/Details.cshtml.cs
+++ src/WebApp/Pages/Users/Details.cshtml.cs
@@ -1,12 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApp.Models;
+using WebApp.Services;
 
-namespace WebApp.Pages.Creators
+namespace WebApp.Pages.Users
 {
     public class Details : PageModel
     {
-        public void OnGet()
+        private readonly IDeveloperService _developerService;
+        private readonly IPostsService _postsService;
+
+        public Details(IDeveloperService developerService, IPostsService postsService)
         {
-            
+            _developerService = developerService;
+            _postsService = postsService;
+        }
+
+        public UserModel UserModel { get; set; }
+        public IEnumerable<ProjectModel> ProjectModels { get; set; }
+        public IEnumerable<PostModel> PostModels { get; set; }
+        
+        public async Task<ActionResult> OnGetAsync(int id)
+        {
+            UserModel = await _developerService.GetUser(id);
+            UserModel.Tags = await _developerService.GetTags(UserModel);
+            ProjectModels = await _developerService.GetUserProjects(id);
+            PostModels = await _postsService.GetUserPosts(id);
+            return Page();
         }
     }
 }
