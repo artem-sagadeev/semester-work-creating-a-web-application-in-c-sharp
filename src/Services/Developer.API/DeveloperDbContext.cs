a223041c4f176e0fe4a4f4027016e4ad5b807f48
Артём Сагадеев <sagadeev.artem2001@gmail.com>
2021-04-05 17:42:27 +0300

    change connection strings

--- src/Services/Developer.API/DeveloperDbContext.cs
+++ src/Services/Developer.API/DeveloperDbContext.cs
@@ -8,6 +8,9 @@ namespace Developer.API
         private const string ConnectionString =
             "Host=localhost;Database=developers_db;Username=postgres;Password=qweasd123";
         
+        private const string HerokuConnectionString =
+            "Host=ec2-54-155-35-88.eu-west-1.compute.amazonaws.com;Database=d8vps29lbcia1r;Username=fytxjwvkkjpewd;Password=1ce4e98e2386cd5e64009fd497bf29f7fc68c309eda345bdf992e22fe40630eb;sslmode=Require;TrustServerCertificate=true";
+        
         public DbSet<User> User { get; set; }
         public DbSet<Company> Company { get; set; }
         public DbSet<Project> Project { get; set; }
@@ -26,7 +29,7 @@ namespace Developer.API
         {
             if (!optionsBuilder.IsConfigured)
             {
-                optionsBuilder.UseNpgsql(ConnectionString);
+                optionsBuilder.UseNpgsql(HerokuConnectionString);
             }
         }
         
