9d770024e446d0d3f6203a83b749c34778e389e7
Ra <galievarailina@gmail.com>
2021-04-19 17:55:10 +0300

    migr

---
+++ src/SubscriptionAPI/Migrations/20210419145343_SubscrAnPaymntApi.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SubscriptionAPI.Migrations
+{
+    public partial class SubscrAnPaymntApi : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Tariffs");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Tariffs",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
