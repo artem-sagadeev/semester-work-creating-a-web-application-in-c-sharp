23cd8b75ad498af276ed19bafd3e26be8b5bf62d
Артём Сагадеев <sagadeev.artem2001@gmail.com>
2021-04-20 16:22:19 +0300

    hot fixes

---
+++ src/Services/Posts.API/Migrations/20210420131851_PostAccess.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Posts.API.Migrations
+{
+    public partial class PostAccess : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RequiredSubscriptionType",
+                table: "Post",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RequiredSubscriptionType",
+                table: "Post");
+        }
+    }
+}
