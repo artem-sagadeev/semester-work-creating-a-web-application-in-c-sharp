50f2a4c55301d18087bbb26f14a5d4587c777734
Артём Сагадеев <sagadeev.artem2001@gmail.com>
2021-05-10 19:52:31 +0300

    some changes

--- src/WebApp/Pages/Feed.cshtml.cs
+++ src/WebApp/Pages/Feed.cshtml.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using WebApp.Models.Identity;
 using WebApp.Models.Posts;
+using WebApp.Models.Subscription;
 using WebApp.Services.Posts;
+using WebApp.Services.Subscription;
 
 namespace WebApp.Pages
 {
@@ -14,12 +17,14 @@ namespace WebApp.Pages
         private readonly IPostsService _postsService;
         private readonly SignInManager<ApplicationUser> _signInManager;
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly ISubscriptionService _subscriptionService;
 
-        public Feed(IPostsService postsService, SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager)
+        public Feed(IPostsService postsService, SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager, ISubscriptionService subscriptionService)
         {
             _postsService = postsService;
             _signInManager = signInManager;
             _userManager = userManager;
+            _subscriptionService = subscriptionService;
         }
 
         public IEnumerable<PostModel> PostModels { get; set; }
@@ -30,7 +35,16 @@ namespace WebApp.Pages
                 return Redirect("/Identity/Account/Login");
 
             var user = await _userManager.GetUserAsync(User);
-            //PostModels =
+            var subscriptions = (await _subscriptionService
+                    .GetPaidSubscriptionsByUserId(user.UserId))
+                    .ToList();
+            var subscriptionsToUsers = subscriptions
+                .Where(s => s.Tariff.TypeOfSubscription == TypeOfSubscription.User);
+            var subscriptionsToProjects = subscriptions
+                .Where(s => s.Tariff.TypeOfSubscription == TypeOfSubscription.Project);
+            var subscriptionsToCompanies = subscriptions
+                .Where(s => s.Tariff.TypeOfSubscription == TypeOfSubscription.Team);
+            
             return Page();
         }
     }
