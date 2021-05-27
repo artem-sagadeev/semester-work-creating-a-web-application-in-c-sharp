0054d38d3bf11bb04ff158a327e66db5e15b4940
Ra <galievarailina@gmail.com>
2021-05-27 11:26:42 +0300

    Fix following

--- src/WebApp/Pages/CompanyProfile.cshtml.cs
+++ src/WebApp/Pages/CompanyProfile.cshtml.cs
@@ -25,16 +25,18 @@ namespace WebApp.Pages
 
         public CompanyModel CompanyModel { get; private set; }
 
-        public async Task OnPostFollowAsync(int userId, int subscribedToId, TypeOfSubscription typeOfSubscription)
+        public async Task<IActionResult> OnPostFollowAsync(int userId, int subscribedToId, TypeOfSubscription typeOfSubscription)
         {
             var handler = new SubscribeHandler(_serviceProvider);
             await handler.Follow(userId, subscribedToId, typeOfSubscription);
+            return Redirect($"/CompanyProfile?id={subscribedToId}");
         }
 
-        public async Task OnPostSubscribeAsync(int subscribedToId, int userId, bool isBasic, bool isImproved, bool isMax, TypeOfSubscription typeOfSubscription)
+        public async Task<IActionResult> OnPostSubscribeAsync(int subscribedToId, int userId, bool isBasic, bool isImproved, bool isMax, TypeOfSubscription typeOfSubscription)
         {
             var handler = new SubscribeHandler(_serviceProvider);
             await handler.Subscribe(userId, subscribedToId, isBasic, isImproved, isMax, typeOfSubscription);
+            return Redirect($"/CompanyProfile?id={subscribedToId}");
         }
 
         public async Task<ActionResult> OnGetAsync(int id)
--- src/WebApp/Pages/UserProfile.cshtml.cs
+++ src/WebApp/Pages/UserProfile.cshtml.cs
@@ -54,16 +54,19 @@ namespace WebApp.Pages
             return Page();
         }
 
-        public async Task OnPostFollowAsync(int userId, int subscribedToId, TypeOfSubscription typeOfSubscription)
+        public async Task<IActionResult> OnPostFollowAsync(int userId, int subscribedToId, TypeOfSubscription typeOfSubscription)
         {
             var handler = new SubscribeHandler(_serviceProvider);
             await handler.Follow(userId, subscribedToId, typeOfSubscription);
+            return Redirect($"/UserProfile?id={subscribedToId}");
         }
 
-        public async Task OnPostSubscribeAsync(int subscribedToId, int userId, bool isBasic, bool isImproved, bool isMax, TypeOfSubscription typeOfSubscription)
+        public async Task<IActionResult> OnPostSubscribeAsync(int subscribedToId, int userId, bool isBasic, bool isImproved, bool isMax, TypeOfSubscription typeOfSubscription)
         {
             var handler = new SubscribeHandler(_serviceProvider);
             await handler.Subscribe(userId, subscribedToId, isBasic, isImproved, isMax, typeOfSubscription);
+            return Redirect($"/UserProfile?id={subscribedToId}");
+
         }
 
         public async Task<IActionResult> OnPostAsync(int id, string text)
