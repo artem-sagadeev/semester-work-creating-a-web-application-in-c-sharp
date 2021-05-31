43c6cd14970461295a731be49783ac2eaab4e60c
Артём Сагадеев <sagadeev.artem2001@gmail.com>
2021-06-01 01:17:39 +0300

    files uploading

---
+++ src/WebApp/wwwroot/files/17_Review.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        
+        public string? Comment { get; set; }
+        
+        public int Rating { get; set; }
+        
+        public DateTime PublicationDate { get; set; }
+        
+        public User? User { get; set; }
+        
+        public Product? Product { get; set; }
+    }
+}
