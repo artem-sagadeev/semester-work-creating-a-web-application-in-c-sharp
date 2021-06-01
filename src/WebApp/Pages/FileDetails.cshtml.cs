914970106d310b9454ae71b85f55df8bb4495f98
Артём Сагадеев <sagadeev.artem2001@gmail.com>
2021-06-02 00:32:23 +0300

    files showing

---
+++ src/WebApp/Pages/FileDetails.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages
+{
+    public class FileDetails : PageModel
+    {
+        public string Code { get; set; }
+        
+        public async Task<IActionResult> OnGet(string name)
+        {
+            using var reader = new StreamReader($"wwwroot/files/{name}");
+            Code = await reader.ReadToEndAsync();
+            return Page();
+        }
+    }
+}
